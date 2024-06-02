720f0bcd288f8da50cd78b7cf282ed12a7019af0
David Okeke <david.okeke@hotmail.co.uk>
2024-06-02 04:56:02 +0100

    Moved static templates into template models directory

--- nodes/modules/system/domain/models/template/index.js
+++ nodes/modules/system/domain/models/template/index.js
@@ -1,10 +1,12 @@
 const area = require("./area");
+const components = require("./components");
 const dynamic = require("./dynamic");
 //const Template = require("./Template");
 const TemplateGenerator = require("./TemplateGenerator");
 
 module.exports = {
     area,
+    components,
     dynamic,
     //Template,
     TemplateGenerator
